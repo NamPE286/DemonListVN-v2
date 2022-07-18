afc1285fcd461c0b59042aea378120b6ef816710
NamPE286 <nambuihung654@gmail.com>
2022-07-18 23:42:55 +0700

    title widget

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,3 +2,34 @@
     import Nav from '../components/Nav.svelte'
 </script>
 <Nav/>
+<div class='titleWidget'>
+    <div class='title'>
+        <p class='bigTitle'>Featured List</p>
+        <p class='description'>Levels created and beaten by Vietnamese</p>
+    </div>
+</div>
+<style lang='scss'>
+    .titleWidget{
+        width: 60%;
+        height: 300px;
+        background-color: #202020;
+        margin-inline: auto;
+        border-radius: 50px;
+        display: flex;
+        align-items: center;
+        .title{
+            margin-left: 80px;
+            font-weight: 500;
+        }
+        .description{
+            font-weight: 200;
+        }
+        p{
+            margin: 0;
+        }
+        .bigTitle{
+            font-size: 50px;
+            margin-bottom: 20px;
+        }
+    }
+</style>
