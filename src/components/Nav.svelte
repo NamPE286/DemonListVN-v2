0e6eba1a2d13287c035ad10eb59c25de7d29331f
NamPE286 <nambuihung654@gmail.com>
2022-07-18 21:18:07 +0700

    delete unnecessary code

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -14,17 +14,13 @@
     }
 </script>
 <div class='topBar'>
-    <div class="menuIconWrapper">
-        <a href='#!' on:click={toggleMenu}>
-            {#if !menuExpanded}
-            <svg id="menuIcon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M3 18v-2h18v2Zm0-5v-2h18v2Zm0-5V6h18v2Z"/></svg>
-            {:else}
-            <svg id='menuIcon' xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M3 18v-2h13v2Zm16.6-1-5-5 5-5L21 8.4 17.4 12l3.6 3.6ZM3 13v-2h10v2Zm0-5V6h13v2Z"/></svg>
-            {/if}
-        </a>
-
-              
-    </div>
+    <a href='#!' class="menuIconWrapper" on:click={toggleMenu}>
+        {#if !menuExpanded}
+        <svg id="menuIcon" xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M3 18v-2h18v2Zm0-5v-2h18v2Zm0-5V6h18v2Z"/></svg>
+        {:else}
+        <svg id='menuIcon' xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M3 18v-2h13v2Zm16.6-1-5-5 5-5L21 8.4 17.4 12l3.6 3.6ZM3 13v-2h10v2Zm0-5V6h13v2Z"/></svg>
+        {/if}
+    </a>
     <p id='title'>Demon List VN</p>
     <a class='submitBtn' href='#!'>
         <p>Submit</p>
