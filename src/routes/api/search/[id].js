69edda0ebb39c33b6b602723906680d6d99221b2
NamPE286 <nambuihung654@gmail.com>
2022-07-22 00:42:24 +0700

    search query with autocorrect

--- src/routes/api/search/[id].js
+++ src/routes/api/search/[id].js
@@ -1,23 +1,42 @@
 import { createClient } from '@supabase/supabase-js'
+import autocorrect from 'autocorrect'
 import 'dotenv/config'
 
 export async function GET({ params }) {
     const supabase = createClient(process.env.API_URL, process.env.API_KEY)
+    //autocorrect all word in params.id
+    var id = params.id.split(' ')
+    for (var i = 0; i < id.length; i++) {
+        id[i] = autocorrect()(id[i])
+    }
+    id = id.join(' ')
     if (isNaN(params.id)) {
+        var d = []
         var { data, error } = await supabase
             .from('levels')
             .select('*')
-            .textSearch('name', params.id, {
+            .textSearch('name', `'${params.id}'`, {
                 type: 'websearch',
                 config: 'english'
             })
+        d = JSON.parse(JSON.stringify(data))
+        var { data, error } = await supabase
+            .from('levels')
+            .select('*')
+            .textSearch('name', `'${id}'`, {
+                type: 'websearch',
+                config: 'english'
+            })
+        d = d.concat(JSON.parse(JSON.stringify(data)))
+        d = d.filter((item, index) => d.indexOf(item) === index)
         return {
             status: 200,
             headers: {
                 'access-control-allow-origin': '*'
             },
             body: {
-                data: data
+                data: d,
+                autoCorrectedWord: id
             }
         };
     }
@@ -32,7 +51,7 @@ export async function GET({ params }) {
                 'access-control-allow-origin': '*'
             },
             body: {
-                data: data
+                data: data,
             }
         };
     }
