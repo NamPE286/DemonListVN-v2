914dad5655dc62839cb6a6d5ce21ae72980a8b4d
NamPE286 <nambuihung654@gmail.com>
2022-07-21 14:46:24 +0700

    email encryption

--- src/routes/api/players/[id]/index.js
+++ src/routes/api/players/[id]/index.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
+import AES from 'crypto-js/aes'
+import enc from 'crypto-js/enc-utf8'
 
 export async function GET({params}) {
     const supabase = createClient('https://qdwpenfblwdmhywwszzj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkd3BlbmZibHdkbWh5d3dzenpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTgzMTk1ODEsImV4cCI6MTk3Mzg5NTU4MX0.b_j2bXu26CYSCMDPei6vGGm9P_QG3I-LsB8OBPm0ZOg')
@@ -6,7 +8,10 @@ export async function GET({params}) {
         .from('players')
         .select('*')
         .eq('id', params.id)
-
+    var a = AES.encrypt(data[0].email, data[0].password).toString()
+    data[0].email = a
+    //var b = AES.decrypt(a, 'a')
+    //console.log(enc.stringify(b))
     return {
         status: 200,
         headers: {
