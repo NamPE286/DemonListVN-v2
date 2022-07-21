f8a2cb15a1d2775904eddf72939365d62a388e42
NamPE286 <nambuihung654@gmail.com>
2022-07-22 00:21:10 +0700

    id search

--- src/routes/api/search/[id].js
+++ src/routes/api/search/[id].js
@@ -1,24 +1,41 @@
 import { createClient } from '@supabase/supabase-js'
-import AES from 'crypto-js/aes'
 import 'dotenv/config'
 
 export async function GET({ params }) {
     const supabase = createClient(process.env.API_URL, process.env.API_KEY)
-    var { data, error } = await supabase
-        .from('levels')
-        .select('*')
-        .textSearch('name', params.id, {
-            type: 'websearch',
-            config: 'english'
-        })
-    return {
-        status: 200,
-        headers: {
-            'access-control-allow-origin': '*'
-        },
-        body: {
-            data: data
-        }
-    };
+    if (isNaN(params.id)) {
+        var { data, error } = await supabase
+            .from('levels')
+            .select('*')
+            .textSearch('name', params.id, {
+                type: 'websearch',
+                config: 'english'
+            })
+        return {
+            status: 200,
+            headers: {
+                'access-control-allow-origin': '*'
+            },
+            body: {
+                data: data
+            }
+        };
+    }
+    else {
+        var { data, error } = await supabase
+            .from('levels')
+            .select('*')
+            .eq('id', params.id)
+        return {
+            status: 200,
+            headers: {
+                'access-control-allow-origin': '*'
+            },
+            body: {
+                data: data
+            }
+        };
+    }
+
 
 }
