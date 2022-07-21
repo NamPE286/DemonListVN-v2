418f0037561260d69f91b0a7f754faa5628bb0e0
NamPE286 <nambuihung654@gmail.com>
2022-07-21 20:51:59 +0700

    dotenv

--- src/routes/api/levels/DL.js
+++ src/routes/api/levels/DL.js
@@ -17,7 +17,7 @@ export async function GET() {
     function getPoint(rank) {
         return roundNumber((100 / Math.sqrt(((rank - 1) / 50) + 0.444444)) - 50, 3);
     }
-    const supabase = createClient('https://qdwpenfblwdmhywwszzj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkd3BlbmZibHdkbWh5d3dzenpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTgzMTk1ODEsImV4cCI6MTk3Mzg5NTU4MX0.b_j2bXu26CYSCMDPei6vGGm9P_QG3I-LsB8OBPm0ZOg')
+    const supabase = createClient(process.env.API_URL, process.env.API_KEY)
     var { data, error } = await supabase
         .from('dl')
         .select('*')
--- src/routes/api/players/[id]/index.js
+++ src/routes/api/players/[id]/index.js
@@ -3,7 +3,7 @@ import AES from 'crypto-js/aes'
 import enc from 'crypto-js/enc-utf8'
 
 export async function GET({params}) {
-    const supabase = createClient('https://qdwpenfblwdmhywwszzj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkd3BlbmZibHdkbWh5d3dzenpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTgzMTk1ODEsImV4cCI6MTk3Mzg5NTU4MX0.b_j2bXu26CYSCMDPei6vGGm9P_QG3I-LsB8OBPm0ZOg')
+    const supabase = createClient(process.env.API_URL, process.env.API_KEY)
     const { data, error } = await supabase
         .from('players')
         .select('*')
