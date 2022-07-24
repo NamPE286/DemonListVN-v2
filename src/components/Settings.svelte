cb5d355eb1b390cc0fc461db4d3d65919adc4552
Truong An Do <truongancpp8@gmail.com>
2022-07-24 09:17:33 +0700

    vb

---
+++ src/components/Settings.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Title from 'Title.svelte';
+</script>
+
+<style lang="scss">
+
+</style>
+
+<Title title="Settings" description="View and modify your preferences"/>
+
