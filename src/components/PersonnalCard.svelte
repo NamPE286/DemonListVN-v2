0af00f67cc4983d2b8d91cf703cdb337480ac5e0
NamPE286 <nambuihung654@gmail.com>
2022-07-25 20:32:32 +0700

    refactor

---
+++ src/components/PersonnalCard.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	export var staffName: string;
+	export var staffYT: string;
+	export var staffFB: string;
+	export var staffGH: string;
+	export var staffAvatar: string;
+</script>
+
+<div class="staff_Card">
+	<div class="c_inlineWrap">
+		<img src={staffAvatar} class="c_avatar" alt="avatar" />
+		<div class="c_flexCol">
+			<p class="c_name">{staffName}</p>
+			<div class="c_flexRow">
+				<!-- svelte-ignore empty-block -->
+				{#if staffYT == "#!" || staffYT == ""}{:else}
+					<a href={staffYT}><img class="c_icon" src="../youtube.svg" alt="youtube" /></a>
+				{/if}
+				<!-- svelte-ignore empty-block -->
+				{#if staffFB == "#!" || staffFB == ""}{:else}
+					<a href={staffFB}><img class="c_icon" src="../facebook.svg" alt="facebook" /></a>
+				{/if}
+				<!-- svelte-ignore empty-block -->
+				{#if staffGH == "#!" || staffGH == ""}{:else}
+					<a href={staffGH}><img class="c_icon" src="../github.svg" alt="github" /></a>
+				{/if}
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	p {
+		margin: 0 0 0 0;
+	}
+	.staff_Card {
+		width: calc(100%);
+		height: 192px;
+		display: flex;
+		align-items: center;
+		border-radius: 48px;
+		margin: 0px auto 16px auto;
+	}
+	.c_inlineWrap {
+		display: flex;
+		flex-direction: row;
+		width: calc(100% - 64px);
+		margin: 0 auto 0 auto;
+	}
+	.c_avatar {
+		height: 128px;
+		width: 128px;
+		border-radius: 64px;
+		margin: 0 32px 0 0;
+	}
+	.c_flexCol {
+		display: flex;
+		flex-direction: column;
+	}
+	.c_flexRow {
+		display: flex;
+		flex-direction: row;
+	}
+	.c_name {
+		font-size: 26px;
+		margin: 16px 0 0 0;
+	}
+	.c_icon {
+		width: 32px;
+		height: 32px;
+		margin-left: 2px;
+		margin-right: 30px;
+		margin-top: 16px;
+	}
+</style>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,18 +1,43 @@
 <script lang="ts">
-    import Title from "../components/Title.svelte";
-    import CardVisit from "../components/PersonnelCard.svelte"
+	import Title from "../components/Title.svelte";
+	import CardVisit from "../components/PersonnalCard.svelte";
 </script>
+
+<div class="pageContent">
+	<Title
+		title="About"
+		description="A list of people who maintain Demon List VN as well as the website developers"
+	/>
+	<p>Staffs</p>
+	<CardVisit
+		staffName="AdminNotDeveloper"
+		staffYT="https://www.youtube.com/channel/UCuAXFkgsw1L7xaCfnd5JJOw"
+		staffFB="https://www.facebook.com/RickAstley"
+		staffGH="#!"
+		staffAvatar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Auto_Racing_Green.svg/1200px-Auto_Racing_Green.svg.png"
+	/>
+  <p>Developers</p>
+	<CardVisit
+		staffName="AdminIsDeveloper"
+		staffYT="https://www.youtube.com/channel/UCuAXFkgsw1L7xaCfnd5JJOw"
+		staffFB="https://www.facebook.com/RickAstley"
+		staffGH="https://github.com/rickastley"
+		staffAvatar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Auto_Racing_Green.svg/1200px-Auto_Racing_Green.svg.png"
+	/>
+</div>
+
 <style lang="scss">
-    p {
-        font-size: 30px;
-        margin: 40px 0 0 32px;
-    }
-    .pageContent {
+	p {
+		font-size: 30px;
+    font-weight: 500;
+		margin: 40px 0 0 32px;
+	}
+	.pageContent {
 		width: 60%;
 		margin-inline: auto;
 		margin-bottom: 100px;
-    }
-    @media screen and (max-width: 1450px) {
+	}
+	@media screen and (max-width: 1450px) {
 		.pageContent {
 			width: 80%;
 		}
@@ -21,11 +46,5 @@
 		.pageContent {
 			width: 90%;
 		}
-    }
+	}
 </style>
-<div class="pageContent">
-    <Title title="About" description="A list of people who maintain Demon List VN as well as the website developers"/>
-    <p>Admin</p>
-    <CardVisit staffName="AdminNotDeveloper" staffYT="https://www.youtube.com/channel/UCuAXFkgsw1L7xaCfnd5JJOw" staffFB="https://www.facebook.com/RickAstley" staffGH="#!" staffAvatar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Auto_Racing_Green.svg/1200px-Auto_Racing_Green.svg.png"/>
-    <CardVisit staffName="AdminIsDeveloper" staffYT="https://www.youtube.com/channel/UCuAXFkgsw1L7xaCfnd5JJOw" staffFB="https://www.facebook.com/RickAstley" staffGH="https://github.com/rickastley" staffAvatar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Auto_Racing_Green.svg/1200px-Auto_Racing_Green.svg.png"/>
-</div>
