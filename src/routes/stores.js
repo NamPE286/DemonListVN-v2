7a2c72ee1348f25bc75467282f16416d75186a45
NamPE286 <nambuihung654@gmail.com>
2022-07-25 23:06:11 +0700

    bug fix

--- src/routes/stores.js
+++ src/routes/stores.js
@@ -8,9 +8,11 @@ var dat1 = {
     data: {}
 }
 dat1.metadata = dat
-fetch(`https://demon-listv2-api.vercel.app/players/3a24c91a-d45f-4e93-83f6-569858aaa35b`)
+if(dat){
+    fetch(`https://demon-listv2-api.vercel.app/players/${dat.id}`)
     .then((response) => response.json())
     .then((data) => {
         dat1.data = data
     });
+}
 export const userdata = writable(dat1);
