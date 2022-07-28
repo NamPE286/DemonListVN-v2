db82d788f4ff0095dc180cbe3fe67f9ca2f65720
NamPE286 <nambuihung654@gmail.com>
2022-07-28 23:39:36 +0700

    about info

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -10,19 +10,54 @@
 	/>
 	<p>Staffs</p>
 	<CardVisit
-		staffName="AdminNotDeveloper"
-		staffYT="https://www.youtube.com/channel/UCuAXFkgsw1L7xaCfnd5JJOw"
-		staffFB="https://www.facebook.com/RickAstley"
+		staffName="Hito73"
+		staffYT="https://www.youtube.com/channel/UCYw-j3PO02FCfEL-M-VZqGg"
+		staffFB="https://www.facebook.com/thecloudsgiveway"
 		staffGH="#!"
-		staffAvatar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Auto_Racing_Green.svg/1200px-Auto_Racing_Green.svg.png"
+		staffAvatar="https://scontent.fhan15-1.fna.fbcdn.net/v/t39.30808-6/239625086_1055808268291685_5337694855111988668_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=z2wYoUfQPwAAX-o3kgY&_nc_ht=scontent.fhan15-1.fna&oh=00_AT9AVDGqJw0h4zcxM7W2c134sunDcmCcJ1uwUr2r_8_VZw&oe=62E71541"
+	/>
+	<CardVisit
+		staffName="noeruchan"
+		staffYT="https://m.youtube.com/c/noeruchangd/featured"
+		staffFB="https://www.facebook.com/profile.php?id=100015875652073"
+		staffGH="#!"
+		staffAvatar="https://yt3.ggpht.com/OBeTi6u8XxufbZxeJRe9jYx9nmBkAdYVBrZLQlke8BQXoMznrmQsoqD5V3K7fHMfx8fJfaeI=s88-c-k-c0x00ffffff-no-rj"
+	/>
+	<CardVisit
+		staffName="shira"
+		staffYT="https://m.youtube.com/channel/UCcdpyP7JZZima9_Ukaguj-w"
+		staffFB="https://www.facebook.com/profile.php?id=100015875652073"
+		staffGH="#!"
+		staffAvatar="https://yt3.ggpht.com/F5UuDHIqnfnMj5LNZEN7RJRgLhq6TEYCVCwqQrLivebh1yTBLJLw_ID6GWeEwwlI_cxHVZTfZg=s88-c-k-c0x00ffffff-no-rj"
+	/>
+	<CardVisit
+		staffName="Kanota"
+		staffYT="https://www.youtube.com/channel/UCyHGYrLHvmZ-5rDTOxcCwAw"
+		staffFB="https://www.facebook.com/Eyetalium"
+		staffGH="#!"
+		staffAvatar="https://yt3.ggpht.com/GDn_2gPVf9lCyTtlTHnPEZ_eTx7OiylIzm67oq48u9LieyvFXnPqIP3FtXqOUYc1DFw3iksyJQ=s88-c-k-c0x00ffffff-no-rj"
 	/>
   <p>Developers</p>
 	<CardVisit
-		staffName="AdminIsDeveloper"
-		staffYT="https://www.youtube.com/channel/UCuAXFkgsw1L7xaCfnd5JJOw"
-		staffFB="https://www.facebook.com/RickAstley"
-		staffGH="https://github.com/rickastley"
-		staffAvatar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Auto_Racing_Green.svg/1200px-Auto_Racing_Green.svg.png"
+		staffName="NamPE"
+		staffYT="https://www.youtube.com/channel/UCCj7J4fxHF70n5zxccZQDww"
+		staffFB="https://www.facebook.com/profile.php?id=100008542323249"
+		staffGH="https://github.com/NamPE286"
+		staffAvatar="https://avatars.githubusercontent.com/u/71075654?v=4"
+	/>
+	<CardVisit
+		staffName="Kryogenic"
+		staffYT="https://youtube.com/channel/UC30N4JkZcgfpC2WBzKJtaMA"
+		staffFB="https://facebook.com/truongan0404"
+		staffGH="https://github.com/DoAn08"
+		staffAvatar="https://avatars.githubusercontent.com/u/106440315?v=4"
+	/>
+	<CardVisit
+		staffName="VnPower"
+		staffYT=""
+		staffFB="https://www.facebook.com/TRVnPower"
+		staffGH="https://github.com/rVnPower"
+		staffAvatar="https://avatars.githubusercontent.com/u/80624167?v=4"
 	/>
 </div>
 
