37ccc906190db6c9673a03b725a8ce50afd3c926
NamPE286 <nambuihung654@gmail.com>
2022-08-05 03:18:55 +0700

    ads

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
 	import Nav from '../components/Nav.svelte';
 </script>
-<head>
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4605218533506777"
-     crossorigin="anonymous"></script>
-</head>
 <Nav />
 
 <slot />
