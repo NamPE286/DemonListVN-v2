75d54102fcbee4072027dbb40f99baed77e1e490
Truong An Do <truongancpp8@gmail.com>
2022-08-19 10:41:33 +0700

    remake error page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,35 +1,48 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    var server:string = "https://discord.gg/hgTSD6eM7D";
 </script>
 <style lang="scss">
     a {
         text-decoration: none;
         color: white;
         font-weight: 600;
-    }
-    a:hover {
         text-decoration: underline;
     }
     p {
         margin: 0 0 4px 0;
     }
     .errorWrapper {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 90%;
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
+        text-align: center;
+        .e_icon {
+            font-size: 120px;
+            font-variation-settings: 'FILL' 1;
+        }
         .e_title {
             font-size: 80px;
-            font-weight: 500;
+            font-weight: 525;
         }
         .e_desc {
             font-family: monospace;
         }
     }
+    .divisor {
+        margin-top: 16px;
+    }
 </style>
 <div class="errorWrapper">
-    <p class="e_title">{$page.status}</p> 
-    <p class="e_desc">{$page.error.message}</p>
-    <p class="e_desc">If you believe this is an error, message a developer <a href={server}>here</a></p>
+    <p class="material-symbols-outlined e_icon">error</p>
+    <p class="e_title">Oops!</p> 
+    <span class="divisor"></span>
+    <p class="e_desc">Error {$page.status} {$page.error.message}</p>
+    <p class="e_desc">If you believe this error should not happen, message a developer <a href="https://discord.gg/hgTSD6eM7D">here</a></p>
     <!-- abc xyz -->
 </div>
