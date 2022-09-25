b03c99b654cb4d9cb47498f2c70fc10440c4d172
NamPE286 <nambuihung654@gmail.com>
2022-09-25 18:29:16 +0700

    menu hide on page change

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -133,7 +133,7 @@
 	/>
 {/if}
 <div class={menuExpanded ? "sidebar1" : "sidebar"} id="sidebarDiv">
-	<a href="/" class="sidebarIconWrapper">
+	<a href="/" class="sidebarIconWrapper" on:click={() => menuExpanded = false}>
 		<div class='hide' id="sidebarIcon">
 			<div class="sidebarIcon">
 				<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M13 9V3h8v6ZM3 13V3h8v10Zm10 8V11h8v10ZM3 21v-6h8v6Zm2-10h4V5H5Zm10 8h4v-6h-4Zm0-12h4V5h-4ZM5 19h4v-2H5Zm4-8Zm6-4Zm0 6Zm-6 4Z"/></svg>			</div>
@@ -141,7 +141,7 @@
 		</div>
 		<p class='hideText' id="iconTitle">Dashboard</p>
 	</a>
-	<a href="/DL" class="sidebarIconWrapper">
+	<a href="/DL" class="sidebarIconWrapper" on:click={() => menuExpanded = false}>
 		<div class='hide' id="sidebarIcon">
 			<div class="sidebarIcon">
 				<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"
@@ -154,7 +154,7 @@
 		</div>
 		<p class='hideText' id="iconTitle">Demon List</p>
 	</a>
-	<a href="/FL" class="sidebarIconWrapper">
+	<a href="/FL" class="sidebarIconWrapper" on:click={() => menuExpanded = false}>
 		<div class='hide' id="sidebarIcon">
 			<div class="sidebarIcon">
 				<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="m8.85 17.825 3.15-1.9 3.15 1.925-.825-3.6 2.775-2.4-3.65-.325-1.45-3.4-1.45 3.375-3.65.325 2.775 2.425ZM5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625 7.2.625-5.45 4.725L18.175 22 12 18.275ZM12 13.25Z"/></svg>
@@ -165,7 +165,7 @@
 	</a>
 	<hr />
 	{#if uid}
-	<a href={`#!`} class="sidebarIconWrapper" on:click={() => window.location.replace(`/player?id=${uid}`)}>
+	<a href={`#!`} class="sidebarIconWrapper" on:click={() => window.location.replace(`/player?id=${uid}`)} on:click={() => menuExpanded = false}>
 		<div class='hide' id="sidebarIcon">
 			<div class="sidebarIcon">
 				<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M5.85 17.1q1.275-.975 2.85-1.538Q10.275 15 12 15q1.725 0 3.3.562 1.575.563 2.85 1.538.875-1.025 1.363-2.325Q20 13.475 20 12q0-3.325-2.337-5.663Q15.325 4 12 4T6.338 6.337Q4 8.675 4 12q0 1.475.488 2.775.487 1.3 1.362 2.325ZM12 13q-1.475 0-2.488-1.012Q8.5 10.975 8.5 9.5t1.012-2.488Q10.525 6 12 6t2.488 1.012Q15.5 8.025 15.5 9.5t-1.012 2.488Q13.475 13 12 13Zm0 9q-2.075 0-3.9-.788-1.825-.787-3.175-2.137-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175 1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138 1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175-1.35 1.35-3.175 2.137Q14.075 22 12 22Zm0-2q1.325 0 2.5-.387 1.175-.388 2.15-1.113-.975-.725-2.15-1.113Q13.325 17 12 17t-2.5.387q-1.175.388-2.15 1.113.975.725 2.15 1.113Q10.675 20 12 20Zm0-9q.65 0 1.075-.425.425-.425.425-1.075 0-.65-.425-1.075Q12.65 8 12 8q-.65 0-1.075.425Q10.5 8.85 10.5 9.5q0 .65.425 1.075Q11.35 11 12 11Zm0-1.5Zm0 9Z"/></svg>
@@ -175,7 +175,7 @@
 		<p class='hideText' id="iconTitle">Profile</p>
 	</a>
 	{/if}
-	<a href="/settings" class="sidebarIconWrapper">
+	<a href="/settings" class="sidebarIconWrapper" on:click={() => menuExpanded = false}>
 		<div class='hide' id="sidebarIcon">
 			<div class="sidebarIcon">
 				<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"
@@ -188,7 +188,7 @@
 		</div>
 		<p class='hideText' id="iconTitle">Settings</p>
 	</a>
-	<a href="/about" class="sidebarIconWrapper">
+	<a href="/about" class="sidebarIconWrapper" on:click={() => menuExpanded = false}>
 		<div class='hide' id="sidebarIcon">
 			<div class="sidebarIcon">
 				<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"
