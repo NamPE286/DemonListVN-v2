38ef207f058fded170f7efad8969a8c91e554b57
NamPE286 <nambuihung654@gmail.com>
2022-10-23 00:07:53 +0700

    rename function

--- src/routes/level.svelte
+++ src/routes/level.svelte
@@ -31,7 +31,7 @@
 		if(level.flPt == 0) return '0pt'
 		return `${level.flPt ? level.flPt : level.dlPt}pt`;
 	}
-	async function removeLevel(item, index) {
+	async function removeRecord(item, index) {
 		fetch(`https://seademonlist-api.vercel.app/record/${item.id}`, {
 			method: 'DELETE',
 			headers: {
@@ -194,7 +194,7 @@
 									/></svg
 								></a
 							>
-							<a href="#!" on:click={() => removeLevel(item, index)}
+							<a href="#!" on:click={() => removeRecord(item, index)}
 								><svg id="forAdmin" xmlns="http://www.w3.org/2000/svg" height="24" width="24"
 									><path
 										d="M7 21q-.825 0-1.412-.587Q5 19.825 5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413Q17.825 21 17 21ZM17 6H7v13h10ZM9 17h2V8H9Zm4 0h2V8h-2ZM7 6v13Z"
