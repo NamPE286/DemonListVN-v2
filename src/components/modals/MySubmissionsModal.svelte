a149f89f12c923269853c89916715766f175d65f
NamPE286 <nambuihung654@gmail.com>
2022-11-04 14:02:36 +0700

    my submission overflow

--- src/components/modals/MySubmissionsModal.svelte
+++ src/components/modals/MySubmissionsModal.svelte
@@ -1,63 +1,76 @@
 <script lang="ts">
-    import { fade } from 'svelte/transition';
+	import { fade } from "svelte/transition";
 	import { createClient } from "@supabase/supabase-js";
 	export var ifShow: boolean;
 	export var uid;
-	var submissions = []
+	var submissions = [];
 	fetch(`https://api.vnpower.tech/player/${uid}/submissions`)
 		.then((response) => response.json())
 		.then((data) => {
 			submissions = data;
 		});
 	const supabase = createClient(import.meta.env.VITE_API_URL, import.meta.env.VITE_API_KEY);
-	async function cancel(item, index){
-		var { data, error } = await supabase
-			.from('submissions')
-			.delete()
-			.match({id: item.id})
-		submissions.splice(index, 1)
-		submissions = submissions
+	async function cancel(item, index) {
+		var { data, error } = await supabase.from("submissions").delete().match({ id: item.id });
+		submissions.splice(index, 1);
+		submissions = submissions;
 	}
-
 </script>
 
 {#if ifShow}
-<div out:fade="{{duration: 200}}" id='abcs'>
-	<div
-		class="dimBg"
-		on:click={() => {
-			ifShow = !ifShow;
-		}}
-        in:fade="{{duration: 150}}"
-	/>
-	<div
-		style="display: flex; justify-content: center; align-items: center; transition: all 0.25s ease-in-out;"
-		class="modalWrapper"
-	>
-		<div class="submitModal s_shadow">
-			<div class="s_flexrow" style="align-items: flex-end;">
-				<p class="s_title s_margin4">My submissions</p>
-			</div>
-			{#each submissions as item, index}
-				<div class="submission">
-					<a id='info' href={item.videoLink} target='_blank'><b id='levelName'>{item.levels.name}</b> <span id='levelID'>(ID: {item.levelid}) ({item.progress}% {item.refreshRate}hz)</span></a>
-					<span id='cancel' class="clickable" on:click={() => cancel(item, index)}>
-						<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M6.4 19 5 17.6l5.6-5.6L5 6.4 6.4 5l5.6 5.6L17.6 5 19 6.4 13.4 12l5.6 5.6-1.4 1.4-5.6-5.6Z"/></svg>
-					</span>
+	<div out:fade={{ duration: 200 }} id="abcs">
+		<div
+			class="dimBg"
+			on:click={() => {
+				ifShow = !ifShow;
+			}}
+			in:fade={{ duration: 150 }}
+		/>
+		<div
+			style="display: flex; justify-content: center; align-items: center; transition: all 0.25s ease-in-out;"
+			class="modalWrapper"
+		>
+			<div class="submitModal s_shadow">
+				<div class="s_flexrow" style="align-items: flex-end;">
+					<p class="s_title s_margin4">My submissions</p>
 				</div>
-			{/each}
-			<div class="s_flexrow buttonWrapper" style="justify-content: flex-end;">
-				<span class="s_button2 s_margin5 s_red clickable" on:click={() => {
-					ifShow = !ifShow;
-				}}>Close</span>
+				<div class="allSubmission">
+					{#each submissions as item, index}
+						<div class="submission">
+							<a id="info" href={item.videoLink} target="_blank"
+								><b id="levelName">{item.levels.name}</b>
+								<span id="levelID"
+									>(ID: {item.levelid}) ({item.progress}% {item.refreshRate}hz)</span
+								></a
+							>
+							<span id="cancel" class="clickable" on:click={() => cancel(item, index)}>
+								<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"
+									><path
+										d="M6.4 19 5 17.6l5.6-5.6L5 6.4 6.4 5l5.6 5.6L17.6 5 19 6.4 13.4 12l5.6 5.6-1.4 1.4-5.6-5.6Z"
+									/></svg
+								>
+							</span>
+						</div>
+					{/each}
+				</div>
+				<div class="s_flexrow buttonWrapper" style="justify-content: flex-end;">
+					<span
+						class="s_button2 s_margin5 s_red clickable"
+						on:click={() => {
+							ifShow = !ifShow;
+						}}>Close</span
+					>
+				</div>
 			</div>
 		</div>
 	</div>
-
-</div>
 {/if}
 
 <style lang="scss">
+	.allSubmission {
+		max-height: 500px;
+		overflow-y: scroll;
+	}
 	.dimBg {
 		position: fixed;
 		margin-top: -135px;
@@ -74,7 +87,7 @@
 	.modalWrapper {
 		z-index: 3;
 	}
-	.submission{
+	.submission {
 		background-color: var(--color21);
 		width: 80%;
 		margin-inline: auto;
@@ -82,18 +95,17 @@
 		display: flex;
 		align-items: center;
 		margin-bottom: 10px;
-		#info{
+		#info {
 			margin-left: 25px;
 			padding-top: 18px;
 			padding-bottom: 18px;
 			color: var(--color6);
 			text-decoration: none;
 		}
-		svg{
-			
+		svg {
 			margin-top: 8px;
 		}
-		#cancel{
+		#cancel {
 			margin-left: auto;
 			margin-right: 25px;
 		}
@@ -138,7 +150,6 @@
 		transition: 0.3s;
 	}
 	.s_shadow {
-		
 	}
 	@media screen and (max-width: 1250px) {
 		.submitModal {
