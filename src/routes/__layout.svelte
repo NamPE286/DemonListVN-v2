95738482c8740e5b61a0e6d5601fdb85d541f0b1
NamPE286 <nambuihung654@gmail.com>
2022-11-06 16:03:54 +0700

    style fix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,53 +1,54 @@
 <script lang="ts">
-	import Nav from '../components/Nav.svelte';
-	import { onMount } from 'svelte';
+	import Nav from "../components/Nav.svelte";
+	import { onMount } from "svelte";
 	var img;
 	var blur, opacity;
-	function getImg(){
-		try{
-			img = localStorage.getItem('bg')
-			blur = localStorage.getItem('blur')
-			opacity = localStorage.getItem('opacity')
-			if(!img) img = ''
-			if(!blur && blur != 0) blur = 0 
-			if(!opacity && opacity != 0) opacity = 100
-		}
-		catch{
-			setTimeout(getImg, 100)
+	function getImg() {
+		try {
+			img = localStorage.getItem("bg");
+			blur = localStorage.getItem("blur");
+			opacity = localStorage.getItem("opacity");
+			if (!img) img = "";
+			if (!blur && blur != 0) blur = 0;
+			if (!opacity && opacity != 0) opacity = 100;
+		} catch {
+			setTimeout(getImg, 100);
 		}
 	}
-	function setTheme(){
-		var currentTheme = localStorage.getItem('theme')
-		if(!currentTheme){
-			currentTheme = 'dark'
-			localStorage.setItem('theme', 'dark')
+	function setTheme() {
+		var currentTheme = localStorage.getItem("theme");
+		if (!currentTheme) {
+			currentTheme = "dark";
+			localStorage.setItem("theme", "dark");
 		}
-		document.documentElement.setAttribute('data-theme', currentTheme)
+		document.documentElement.setAttribute("data-theme", currentTheme);
 	}
 	onMount(() => {
-		getImg()
-		setTheme()
-	})
-	
+		getImg();
+		setTheme();
+	});
 </script>
+
 <svelte:head>
 	<!-- Google tag (gtag.js) -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-8T6SG5ZBDC"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-		gtag('config', 'G-8T6SG5ZBDC');
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag("js", new Date());
+		gtag("config", "G-8T6SG5ZBDC");
 	</script>
 </svelte:head>
 {#if img}
-	<img src={img} alt='bg' style={`filter: blur(${blur}px); opacity: ${opacity}%;`}>
+	<img src={img} alt="bg" style={`filter: blur(${blur}px); opacity: ${opacity}%;`} />
 {/if}
 <Nav />
 <slot />
 
 <style lang="scss">
-	img{
+	img {
 		position: fixed;
 		top: 0;
 		left: 0;
@@ -55,5 +56,10 @@
 		width: 100%;
 		height: 100%;
 		object-fit: cover;
+	}
+	:global(.pageContent) {
+		width: 60%;
+		margin-inline: auto;
+		margin-bottom: 100px;
 	}
 </style>
