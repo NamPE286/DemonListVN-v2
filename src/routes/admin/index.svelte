fd15da4456e246959c4b270a4d181b76b1f55f8a
NamPE286 <nambuihung654@gmail.com>
2022-11-10 19:39:24 +0700

    fix update list

--- src/routes/admin/index.svelte
+++ src/routes/admin/index.svelte
@@ -1,38 +1,39 @@
 <script>
-    import { createClient } from "@supabase/supabase-js";
+	import { createClient } from "@supabase/supabase-js";
 	import Title from "../../components/widgets/Title.svelte";
 	import { userdata } from "../stores";
 	const supabase = createClient(import.meta.env.VITE_API_URL, import.meta.env.VITE_API_KEY);
-	var a = 'Refresh list'
-	async function refreshList(){
-		a = 'Refreshing...'
-		const res = await (
-					await fetch(`https://api.vnpower.tech/refreshList`, {
-						method: "POST",
-						headers: {
-							"Content-Type": "application/json"
-						},
-						body: JSON.stringify({
-							token: supabase.auth.session().access_token
-						})
-					})
-				).json();
+	var a = "Refresh list";
+	async function refreshList() {
+		a = "Refreshing...";
+		await fetch(`https://api.vnpower.tech/refreshList`, {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				token: supabase.auth.session().access_token
+			})
+		});
+		a = "Refreshed!"
 	}
 </script>
+
 {#if $userdata.data.isAdmin}
 	<div class="pageContent">
 		<Title title="Admin" description="" />
-		<button class='refreshList' on:click={refreshList}>{a}</button><br>
+		<button class="refreshList" on:click={refreshList}>{a}</button><br />
 		<span>Submit checker: </span>
-		<a href='/admin/submitChecker'>All</a>
-		<a href='/admin/submitChecker/DL'>DL</a>
-		<a href='/admin/submitChecker/FL'>FL</a><br>
-		<a href='/admin/allPlayer'>New levels</a><br>
-		<a href='/admin/allPlayer'>Player manager</a>
+		<a href="/admin/submitChecker">All</a>
+		<a href="/admin/submitChecker/DL">DL</a>
+		<a href="/admin/submitChecker/FL">FL</a><br />
+		<a href="/admin/allPlayer">New levels</a><br />
+		<a href="/admin/allPlayer">Player manager</a>
 	</div>
 {/if}
+
 <style lang="scss">
-	.refreshList{
+	.refreshList {
 		margin-top: 50px;
 		height: 25px;
 		width: 100px;
