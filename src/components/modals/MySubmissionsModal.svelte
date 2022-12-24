81f861130a3f3a3b4aa4f48d6bdf7b0dab99183a
NamPE286 <nambuihung654@gmail.com>
2022-12-25 01:09:07 +0700

    no submission text

--- src/components/modals/MySubmissionsModal.svelte
+++ src/components/modals/MySubmissionsModal.svelte
@@ -4,10 +4,12 @@
 	export var ifShow: boolean;
 	export var uid;
 	var submissions = [];
+	var hasSubmission = true
 	fetch(`https://api.vnpower.tech/player/${uid}/submissions`)
 		.then((response) => response.json())
 		.then((data) => {
 			submissions = data;
+			hasSubmission = submissions.length != 0
 		});
 	const supabase = createClient(import.meta.env.VITE_API_URL, import.meta.env.VITE_API_KEY);
 	async function cancel(item, index) {
@@ -36,25 +38,32 @@
 				<div class="s_flexrow" style="align-items: flex-end;">
 					<p class="s_title s_margin4">My submissions</p>
 				</div>
-				<div class="allSubmission">
-					{#each submissions as item, index}
-						<div class="submission">
-							<a id="info" href={item.videoLink} target="_blank"
-								><b id="levelName">{item.levels.name}</b>
-								<span id="levelID"
-									>(ID: {item.levelid}) ({item.progress}% {item.refreshRate}hz)</span
-								></a
-							>
-							<span id="cancel" class="clickable" on:click={() => cancel(item, index)}>
-								<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"
-									><path
-										d="M6.4 19 5 17.6l5.6-5.6L5 6.4 6.4 5l5.6 5.6L17.6 5 19 6.4 13.4 12l5.6 5.6-1.4 1.4-5.6-5.6Z"
-									/></svg
+				{#if hasSubmission}
+					<div class="allSubmission">
+						{#each submissions as item, index}
+							<div class="submission">
+								<a id="info" href={item.videoLink} target="_blank"
+									><b id="levelName">{item.levels.name}</b>
+									<span id="levelID"
+										>(ID: {item.levelid}) ({item.progress}% {item.refreshRate}hz)</span
+									></a
 								>
-							</span>
-						</div>
-					{/each}
-				</div>
+								<span id="cancel" class="clickable" on:click={() => cancel(item, index)}>
+									<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"
+										><path
+											d="M6.4 19 5 17.6l5.6-5.6L5 6.4 6.4 5l5.6 5.6L17.6 5 19 6.4 13.4 12l5.6 5.6-1.4 1.4-5.6-5.6Z"
+										/></svg
+									>
+								</span>
+							</div>
+						{/each}
+					</div>
+				{/if}
+				{#if !hasSubmission}
+					<div class='noSubmissionTextWrapper'>
+						<p class='noSubmissionText'>No submission</p>
+					</div>
+				{/if}
 				<div class="s_flexrow buttonWrapper" style="justify-content: flex-end;">
 					<span
 						class="s_button2 s_margin5 s_red clickable"
@@ -69,6 +78,13 @@
 {/if}
 
 <style lang="scss">
+	.noSubmissionTextWrapper{
+		display: flex;
+	}
+	.noSubmissionText{
+		color: gray;
+		margin-inline: auto;
+	}
 	.allSubmission {
 		max-height: 500px;
 		overflow-y: scroll;
