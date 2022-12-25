e387891c9ab75dcad281f10c99fd88260f05bf3c
NamPE286 <nambuihung654@gmail.com>
2022-12-26 00:10:36 +0700

    title design update

--- src/components/widgets/Title.svelte
+++ src/components/widgets/Title.svelte
@@ -15,27 +15,38 @@
 <style lang="scss">
 	.titleWidget {
 		grid-area: header;
-		width: 100%;
-		min-height: 300px;
-		background-color: var(--color23);
+		width: 120%;
+		margin-left: -10%;
+		min-height: 350px;
 		border-radius: 50px;
 		display: flex;
 		align-items: center;
+		justify-content: center;
 		margin-bottom: -25px;
+		background-color: var(--color26);
+		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+		backdrop-filter: blur(10.8px);
+		-webkit-backdrop-filter: blur(10.8px);
+		border: 1px solid rgba(255, 255, 255, 0.11);
 		.title {
 			margin-inline: 80px;
 			font-weight: 500;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
 		}
 		.description {
-			font-weight: 200;
+			font-weight: 300;
+			font-size: 21px;
 		}
 		p {
 			margin: 0;
 		}
 		.bigTitle {
 			font-weight: 500;
-			font-size: 50px;
-			margin-bottom: 20px;
+			font-size: 100px;
+			margin-bottom: 10px;
 			margin-top: 0;
 		}
 	}
