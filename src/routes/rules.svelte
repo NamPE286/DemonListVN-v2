138246d56613f34ba45a28645437e19bb6496ab4
NamPE286 <nambuihung654@gmail.com>
2023-01-01 09:49:09 +0700

    typo fix

--- src/routes/rules.svelte
+++ src/routes/rules.svelte
@@ -18,7 +18,7 @@
             <li>Do not turn off orb pulse: You must playing the level with downloaded music (any song is acceptable).</li>
             <li>For mobile player: Do not hiding trail in your completion or your record will be rejected.</li>
         </ul>
-        <h2>Featured List rule</h2>
+        <h2>Featured List rules</h2>
         <ul>
             <li>Only 100% records of any demons in the list are accepted (Only apply for FDLVN).</li>
             <li>Any records related to any cheat tools or secret ways will be rejected (However, FPS bypass is allowed, up to 360 FPS).</li>
