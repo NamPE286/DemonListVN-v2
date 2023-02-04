b656815c58ad734c3227792ff921e16168936456
NamPE286 <nambuihung654@gmail.com>
2023-02-04 23:08:07 +0700

    update rule

--- src/routes/rules.svelte
+++ src/routes/rules.svelte
@@ -9,26 +9,27 @@
 <Title title="Rules" description="Read the rules" />
 <div class="pageContent">
     <div class='rule'>
+        <h2>General rules</h2>
+        <ul>
+            <li>Records must reach minimum progress requirement of any level to be accepted.</li>
+            <li>Cheats and tools are not allowed except for these:</li>
+            <ul>
+                <li>FPS bypass (Max 360fps).</li>
+            </ul>
+            <li>Records stolen from other are not accepted.</li>
+            <li>Secret routes or bug routes are not allowed.</li>
+            <li>Record must have sound.</li>
+            <li>Icon trail must be visible.</li>
+        </ul>
         <h2>Demon List rules</h2>
         <ul>
             <li>You must have Vietnamese citizenship.</li>
             <li>If you got banned from GDVN or CDGDVN facebook group, your record will be rejected until you got unbanned.</li>
-            <li>Submitted records can correspond to a player completing a level or achieving progress above its minimum requirement. Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps).</li>
-            <li>Secret routes or bug routes are not allowed.</li>
-            <li>Do not turn off orb pulse: You must playing the level with downloaded music (any song is acceptable).</li>
-            <li>For mobile player: Do not hiding trail in your completion or your record will be rejected.</li>
-        </ul>
-        <h2>Featured List rules</h2>
-        <ul>
-            <li>Only 100% records of any demons in the list are accepted (Only apply for FDLVN).</li>
-            <li>Any records related to any cheat tools or secret ways will be rejected (However, FPS bypass is allowed, up to 360 FPS).</li>
-            <li>Stealing videos from others will not be accepted.</li>
         </ul>
         <h2>Rating</h2>
         <ul>
-            <li>Rating is a metric intended to more accurately gauge player performance and skill.</li>
+            <li>Rating is a metric represents player potential to beat levels in their rating range.</li>
             <li>Rating is based on player's best performance and performance on other levels on Demon List (Note: grinding is not a viable way to increase player's rating)</li>
-            <li>Rating also represents player potential to beat levels in their rating range</li>
             <li>Rating distribution:</li>
             <ul>
                 <li><Badge rating={250}> Novice: >= 250</Badge></li>
